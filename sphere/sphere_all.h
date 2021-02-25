74c3d5d088296c3342d10b923c556d22a1946be2
dimied <dimidimidi@googlemail.com>
2021-02-25 18:49:18 +0100

    Order of constants redefined

---
+++ sphere/sphere_all.h
@@ -0,0 +1,36 @@
+#ifndef __PASULI_SPHERE_ALL_SURFACES__
+#define __PASULI_SPHERE_ALL_SURFACES__
+
+#include "ellipsoid.h"
+#include "open_sphere.h"
+#include "pseudosphere.h"
+#include "sphere_v.h"
+#include "sphere_uv.h"
+#include "twisted_sphere.h"
+#include "wave_sphere.h"
+#include "wrinkle_sphere.h"
+#include "wrinkle_sphere2.h"
+
+#if (COMPILE_DESC_SPHERE != 0)
+extern char *descSphere;
+extern char *descEllipsoid;
+extern char *descSphereV;
+extern char *descOpenSphere;
+extern char *descSphereUV;
+extern char *descTwistedSphere;
+extern char *descWaveSphere;
+extern char *descPseudoSphere;
+#endif
+
+#if (COMPILE_DEF_DESC_SPHERE != 0)
+extern PaSuLiDefDesc pslddSphere;
+extern PaSuLiDefDesc pslddEllipsoid;
+extern PaSuLiDefDesc pslddSphereV;
+extern PaSuLiDefDesc pslddOpenSphere;
+extern PaSuLiDefDesc pslddSphereUV;
+extern PaSuLiDefDesc pslddTwistedSphere;
+extern PaSuLiDefDesc pslddWaveSphere;
+extern PaSuLiDefDesc pslddPseudoSphere;
+#endif
+
+#endif
